473daf57e647044113be4d699819fc968e923b4c
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-02-16 23:38:54 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = "11980417"  
+    API_HASH = "0c04d148edcc0fb5ee0ef2254f7c1bc6"
+    BOT_TOKEN = "5285771505:AAF_6NgtrgBMjeBxq8eeoIuXTzXRIXIPg-I"
