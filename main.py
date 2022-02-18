676a092c5949b4dcd4ad6d5ac7e00a0e9c98e019
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-02-18 16:47:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def a(client, message):
             title = results[0]["title"]
             thumbnail = results[0]["thumbnails"][0]
             duration = results[0]["duration"]
-            katy = f"<a href=https://t.me/katy_perry_musicbot>Katy Perry</a>""
+            katy = "<a href=https://t.me/katy_perry_musicbot>Katy Perry</a>""
 
             #UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
             if time_to_seconds(duration) >= 1800:  # duration limit
