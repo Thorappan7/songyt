1f857693fe2bff3a1fd678cca97cde0837df05c1
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-02-18 23:59:30 +0530

    Update text.py

--- text.py
+++ text.py
@@ -13,25 +13,25 @@ class Text (object):
 
     HELP_TXT = """ <b>HELP TEXT</b> 
 
-Hey {}, its very easy to request music !
+Hey {}, its very easy to request music..!
 
 ➙ This command works only in group 
-➙ just send me /song [song name] or /song [ YouTube link ]
+➙ just send me /song [song name] or 
+   /song [ YouTube link ]
 
 ➙ Eg :<code>/song darshana </code>
+      <code>/song www.youtube.com/... </code>
 
 """
     REP_TXT = """✒ Title : {title[:35]}\n✒ Duration : {duration} \n✒ Uploaded By : <a href="https://t.me/katy_perry_musicbot"> Katy Perry </a>
 """
        
     EG_TXT = """
-            Examples 
-====================================
-
+           Examples 
+=================================
   /song darshana Hridayam
   /song alone marshmallow
   /song Arabic Kuthu
-  /song https://www.youtube.com....
-
-====================================
+  /song https://www.youtube.com..
+=================================
 """
