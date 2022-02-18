aab53f74c24adf17f1ab3ddfb14e8f40ae4a5491
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-02-18 14:42:05 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,17 @@
+class Text (object):
+
+
+    START_TXT = """<b>Hey {}!!</b>
+<i>I'm Just a Telegram Bot</i> <a href=https://t.me/thorappan_bots>Thorappan</a> <i> He made me for Managing Telegram Group with Manuel filter Support. Check Help button for More info
+anyway welcome To Ramanan Bot </i>"""
+
+    ABOUT_TXT = """
+<b>🧑‍🎤 Name</b> : <code> Katy Perry </code>   
+<b>👽 Creator</b> : <b><a href="https://t.me/thorappan_bots">Thorappan 🧙‍♂</b></a>
+<b>⌨️ Language</b> : <code>Python3 📇</code>
+<b>📟 Library</b> : <a href="https://docs.pyrogram.org">Pyrogram  </a>
+<b>💵 Source Code</b> : <a href="https://telegra.ph/file/85f5432b72264822bd051.jpg">🏃 Source Code</a>
+"""
+
+    HELP_TXT = """ I'm not a perfect Version, but i can do some work I hope you guys enjoy ..🥰
+    """
